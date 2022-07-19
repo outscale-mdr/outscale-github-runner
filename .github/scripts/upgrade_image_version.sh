615a1871c3b2195b3d7a97784e4bcee4bd3400f9
Maxime Dufour <maxime.dufour@outscale.com>
2022-07-19 09:28:23 +0000

    test auto-upgrade

---
+++ .github/scripts/upgrade_image_version.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e 
+
+CURRENT_VERSION="$1"
+UPGRADE_VERSION="$2"
+
+root=$(cd "$(dirname $0)/../.." && pwd)
+
+sed -ri "s|myoung34/github-runner:${CURRENT_VERSION}-ubuntu-bionic|myoung34/github-runner:${UPGRADE_VERSION}-ubuntu-bionic|g" "${root}/terraform/env_file/etc/systemd/system/github-runners.service"
+
+branch_name="autoupgrade-${UPGRADE_VERSION}"
+git branch -m $branch_name
+
+
+# setup git && commit
+git config user.name "Outscale Bot"
+git config user.email "opensource+bot@outscale.com"
+cd "$root"
+git add "terraform/env_file/etc/systemd/system/github-runners.service"
+git commit -m "Upgrade github-runner to ${UPGRADE_VERSION}"
+
+
+# Push the commit
+if [ -z "$SSH_PRIVATE_KEY" ]; then
+    echo "SSH_PRIVATE_KEY is missing, abort."
+    exit 1
+fi
+echo "$SSH_PRIVATE_KEY" > "$root/bot.key"
+GIT_SSH_COMMAND="ssh -i $root/bot.key" git push -f origin $branch_name
+
+
+if [ -z "$GH_TOKEN" ]; then
+    echo "GH_TOKEN is missing, abort."
+    exit 1
+fi
+
+# https://docs.github.com/en/free-pro-team@latest/rest/reference/pulls#create-a-pull-request
+result=$(curl -s -X POST -H "Authorization: token $GH_TOKEN" -d "{\"head\":\"$branch_name\",\"base\":\"main\",\"title\":\"Upgrade Github Runner ($UPGRADE_VERSION)\",\"body\":\"Automatic upgrade of Github Runner to $UPGRADE_VERSION\"}" "https://api.github.com/repos/outscale-dev/outscale-github-runner/pulls")
+
+errors=$(echo $result | jq .errors)
+
+if [ "$errors" != "null" ]; then
+    echo "errors while creating pull request, abort."
+    exit 1
+fi
